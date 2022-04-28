5277c5b2c096f9d6fff886a65167c21e342d040a
david <davidperezmolina.dev@gmail.com>
2022-04-28 18:13:03 +0200

    User Service Added. Closes #6

---
+++ EbisuWebApi.Application.Services/Contracts/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using EbisuWebApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbisuWebApi.Application.Services.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+
+        Task<IEnumerable<UserDTO>> AddUserAsync(UserDTO userDTO);
+    }
+}
--- EbisuWebApi.Infrastructure.Repositories/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EbisuWebApi.Infrastructure.Repositories
-{
-    public class Class1
-    {
-
-    }
-}
