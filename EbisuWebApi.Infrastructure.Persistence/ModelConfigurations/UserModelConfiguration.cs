ed846cd0255e5b26938a7f0d9da12fdd29c39b47
Rafael Gayoso <gayoso0597@gmail.com>
2022-04-28 14:08:05 +0200

    created persistence for Database closes #13

---
+++ EbisuWebApi.Infrastructure.Persistence/ModelConfigurations/UserModelConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EbisuWebApi.Infrastructure.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingCF.Infrastructure.Repositories.Configurations
+{
+    public class UserModelConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(user => user.UserId);
+            builder.Property(user => user.UserId).UseMySqlIdentityColumn();
+            builder.Property(user => user.UserName).IsRequired().HasMaxLength(255);
+            builder.Property(user => user.Email).IsRequired().HasMaxLength(255);
+            builder.Property(user => user.Password).IsRequired().HasMaxLength(255);
+        }
+    }
+}
