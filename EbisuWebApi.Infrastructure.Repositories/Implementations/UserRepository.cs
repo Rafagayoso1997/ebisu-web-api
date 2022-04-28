19dceec43a417f39b133d891bdbbd0f635caea83
david <davidperezmolina.dev@gmail.com>
2022-04-28 18:20:11 +0200

    User Repository Added. Closes #22

---
+++ EbisuWebApi.Infrastructure.Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using EbisuWebApi.Domain.Entities;
+using EbisuWebApi.Domain.RepositoryContracts.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbisuWebApi.Infrastructure.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task<UserEntity> Add(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserEntity> Delete(UserEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserEntity> GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserEntity> Update(UserEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
