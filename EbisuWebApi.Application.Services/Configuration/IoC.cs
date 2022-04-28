ebd2c465627e00ccb92e5de69f23bf4e5376567a
david <davidperezmolina.dev@gmail.com>
2022-04-28 17:58:33 +0200

    Dependency Injection added. Closes #16

---
+++ EbisuWebApi.Application.Services/Configuration/IoC.cs
@@ -0,0 +1,27 @@
+﻿using EbisuWebApi.Domain.RepositoryContracts.Contracts;
+using EbisuWebApi.Infrastructure.Persistence.DataBaseContext;
+using EbisuWebApi.Infrastructure.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbisuWebApi.Application.Services.Configuration
+{
+    public static class IoC
+    {
+        public static IServiceCollection ConfigureServicesLayer(this IServiceCollection services)
+        {
+            
+            var connectionString = "";
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            services.AddTransient<IUserRepository, UserRepository>();
+            return services;
+        }
+    }
+}
--- EbisuWebApi.Web.Api/Program.cs
+++ EbisuWebApi.Web.Api/Program.cs
@@ -1,8 +1,16 @@
+using EbisuWebApi.Application.Services.Configuration;
+using EbisuWebApi.Application.Services.Contracts;
+using EbisuWebApi.Application.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.ConfigureServicesLayer();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
